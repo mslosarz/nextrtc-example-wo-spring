1e570ac4f9dad96598922e2cb7874a6ec849e7d3
Marcin Ślósarz <m.slosarz@ahej.pl>
2018-06-20 22:36:13 +0200

    RC5 implemented

--- src/main/java/org/nextrtc/examples/videochat/CustomHandler.java
+++ src/main/java/org/nextrtc/examples/videochat/CustomHandler.java
@@ -1,14 +1,16 @@
 package org.nextrtc.examples.videochat;
 
-import org.apache.log4j.Logger;
+
 import org.nextrtc.signalingserver.api.NextRTCEvents;
 import org.nextrtc.signalingserver.api.NextRTCHandler;
 import org.nextrtc.signalingserver.api.annotation.NextRTCEventListener;
 import org.nextrtc.signalingserver.api.dto.NextRTCEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @NextRTCEventListener({NextRTCEvents.SESSION_OPENED, NextRTCEvents.MEMBER_JOINED, NextRTCEvents.MEMBER_LEFT})
 class CustomHandler implements NextRTCHandler {
-    private static final Logger log = Logger.getLogger(CustomHandler.class);
+    private static final Logger log = LoggerFactory.getLogger(CustomHandler.class);
 
     @Override
     public void handleEvent(NextRTCEvent event) {
--- src/main/java/org/nextrtc/examples/videochat/MyEndpoint.java
+++ src/main/java/org/nextrtc/examples/videochat/MyEndpoint.java
@@ -1,28 +1,22 @@
 package org.nextrtc.examples.videochat;
 
-import org.apache.log4j.Logger;
-import org.nextrtc.signalingserver.api.ConfigurationBuilder;
-import org.nextrtc.signalingserver.api.EndpointConfiguration;
-import org.nextrtc.signalingserver.api.NextRTCEndpoint;
-import org.nextrtc.signalingserver.codec.MessageDecoder;
-import org.nextrtc.signalingserver.codec.MessageEncoder;
+import org.nextrtc.signalingserver.api.NextRTCServer;
+import org.nextrtc.signalingserver.domain.Connection;
 import org.nextrtc.signalingserver.domain.InternalMessage;
 import org.nextrtc.signalingserver.domain.Signal;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import javax.websocket.*;
 import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
 
 
-@ServerEndpoint(value = "/signaling",
-        decoders = MessageDecoder.class,
-        encoders = MessageEncoder.class)
-public class MyEndpoint extends NextRTCEndpoint {
+@ServerEndpoint(value = "/signaling")
+public class MyEndpoint {
 
-    private static final Logger log = Logger.getLogger(MyEndpoint.class);
-
-    protected EndpointConfiguration manualConfiguration(ConfigurationBuilder builder) {
-        log.info("Manual configuration done");
-        EndpointConfiguration configuration = builder.createDefaultEndpoint();
-
+    private static final Logger log = LoggerFactory.getLogger(MyEndpoint.class);
+    private static final NextRTCServer server = NextRTCServer.create(configuration -> {
         configuration.nextRTCProperties().setPingPeriod(1);
 
         configuration.signalResolver().addCustomSignal(Signal.fromString("upperCase"), (msg) ->
@@ -34,6 +28,59 @@ public class MyEndpoint extends NextRTCEndpoint {
 
         configuration.eventDispatcher().addListener(new CustomHandler());
         return configuration;
+    });
+
+    private static class SessionWrapper implements Connection {
+
+        private final Session session;
+
+        public SessionWrapper(Session session) {
+            this.session = session;
+        }
+
+        @Override
+        public String getId() {
+            return session.getId();
+        }
+
+        @Override
+        public boolean isOpen() {
+            return session.isOpen();
+        }
+
+        @Override
+        public void sendObject(Object object) {
+            try {
+                session.getBasicRemote().sendText(NextRTCServer.MessageEncoder.encode(object));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    @OnOpen
+    public void onOpen(Session session, EndpointConfig config) {
+        log.info("Opening: " + session.getId());
+        server.register(new SessionWrapper(session));
+    }
+
+    @OnMessage
+    public void onMessage(String message, Session session) {
+        log.debug("Handling message from: " + session.getId());
+        server.handle(NextRTCServer.MessageDecoder.decode(message), new SessionWrapper(session));
+    }
+
+    @OnClose
+    public void onClose(Session session, CloseReason reason) {
+        log.info("Closing: " + session.getId() + " with reason: " + reason.getReasonPhrase());
+        server.unregister(new SessionWrapper(session), reason.getReasonPhrase());
+    }
+
+    @OnError
+    public void onError(Session session, Throwable exception) {
+        log.error("Occured exception for session: " + session.getId() + ", reason: " + exception.getMessage());
+        log.debug("Endpoint exception: ", exception);
+        server.handleError(new SessionWrapper(session), exception);
     }
 
 }
